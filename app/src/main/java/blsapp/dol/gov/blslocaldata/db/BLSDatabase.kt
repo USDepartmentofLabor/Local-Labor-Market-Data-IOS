9d760ed001a9d3efbdb2d7c614da6ee415532684
Greg Gruse <ggg>
2019-01-26 17:51:00 -0500

    If this is the first install - copy over the DB from the bundle

--- app/src/main/java/blsapp/dol/gov/blslocaldata/db/BLSDatabase.kt
+++ app/src/main/java/blsapp/dol/gov/blslocaldata/db/BLSDatabase.kt
@@ -5,14 +5,22 @@ import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
+import android.util.Log
 import blsapp.dol.gov.blslocaldata.db.dao.*
 import blsapp.dol.gov.blslocaldata.db.entity.*
+import blsapp.dol.gov.blslocaldata.ui.search.SearchActivity
+import java.io.FileOutputStream
+import java.io.IOException
 import java.util.concurrent.Executors
+import java.nio.file.Files.exists
+
+
 
 @Database(entities = arrayOf(ZipCountyEntity::class, ZipCbsaEntity::class, NationalEntity::class,
         MetroEntity::class, StateEntity::class, CountyEntity::class,
         MSACountyEntity::class), version = 1)
 abstract class BLSDatabase: RoomDatabase() {
+
     abstract fun zipCountyDAO(): ZipCountyDao
     abstract fun zipCbsaDAO(): ZipCbsaDao
     abstract fun metroDAO(): MetroDao
@@ -24,25 +32,26 @@ abstract class BLSDatabase: RoomDatabase() {
     companion object {
         @Volatile
         private var INSTANCE: BLSDatabase? = null
+        private val TAG = SearchActivity::class.java.simpleName
+        private val DB_NAME = "BLSDatabase.db"
 
         fun getInstance(context: Context): BLSDatabase {
             val tempInstance = INSTANCE
             if (tempInstance != null) {
                 return tempInstance
             }
+
             synchronized(this) {
+
+                val dbPath = context.getDatabasePath(DB_NAME)
+                if (!dbPath.exists()) {
+                    copyAttachedDatabase(context, DB_NAME)
+                }
+
                 val instance = Room.databaseBuilder(context.applicationContext,
                         BLSDatabase::class.java,
-                        "BLSDatabase.db")
+                        DB_NAME)
                         .fallbackToDestructiveMigration()
-                        .addCallback(object : Callback() {
-                            override fun onCreate(db: SupportSQLiteDatabase) {
-                                super.onCreate(db)
-                                Executors.newSingleThreadExecutor().execute {
-//                                    fillInDB(context, getInstance(context = appContext))
-                                }
-                            }
-                        })
                         .build()
                 INSTANCE = instance
                 return instance
@@ -51,6 +60,33 @@ abstract class BLSDatabase: RoomDatabase() {
 
         private fun fillInDB(context: Context, db: BLSDatabase) {
             LoadDataUtil.preloadDB(context, db)
+        }
+
+        private fun copyAttachedDatabase(context: Context, databaseName: String) {
+            val dbPath = context.getDatabasePath(databaseName)
+            // Make sure we have a path to the file
+            dbPath.parentFile.mkdirs()
+
+            // Try to copy database file
+            try {
+                val inputStream = context.assets.open("$databaseName")
+                val output = FileOutputStream(dbPath)
+
+                val buffer = ByteArray(8192)
+
+                while (inputStream.read(buffer) > 0) {
+                    output.write(buffer)
+                    Log.d("#DB", "writing>>")
+                }
+
+                output.flush()
+                output.close()
+                inputStream.close()
+            } catch (e: IOException) {
+                Log.d(TAG, "Failed to open file", e)
+                e.printStackTrace()
+            }
+
         }
     }
 }
